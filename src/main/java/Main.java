3e65b3c1e7a6a58f383f974297a5136b08209929
Mohammed-BENHAMMOUTE <mohammedbenhammoute@gmail.com>
2025-06-11 00:01:57 +0100

    ----

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.Array;
+import java.util.List;
 
 public class Main {
   public static void main(String[] args){
@@ -16,6 +18,7 @@ public class Main {
        serverSocket.setReuseAddress(true);
     //   // Wait for connection from client.
        clientSocket = serverSocket.accept();
+       clientSocket.getOutputStream().write(new byte[]{0,0,0,0,0,0,0,7});
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
      } finally {
