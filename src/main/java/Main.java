987e9a751d1e7cffc6af075c47b2a0aecbd60c02
Mohammed-BENHAMMOUTE <mohammedbenhammoute@gmail.com>
2025-06-10 23:13:31 +0100

    ---

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,6 @@ public class Main {
   public static void main(String[] args){
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.err.println("Logs from your program will appear here!");
-
      ServerSocket serverSocket = null;
      Socket clientSocket = null;
      int port = 9092;
